067f90e79456c38724f9d2d986e9f7a5b3fb9b5f
HM <hello@hazella.co>
2019-03-02 00:59:57 -0600

    Business logic added

--- app/src/main/java/com/hm/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/hm/beeradviser/FindBeerActivity.java
@@ -3,6 +3,8 @@ package com.hm.beeradviser;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends Activity {
 
@@ -14,6 +16,18 @@ public class FindBeerActivity extends Activity {
 
     // Called when the user clicks the button.
     public void onClickFindBeer(View view) {
+
+        // Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        // Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        // Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        // Display the selected item
+        brands.setText(beerType);
 
     }
 }
