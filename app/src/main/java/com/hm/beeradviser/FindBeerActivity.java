52e4fd27078b5f8c617aa83f14494769734f04e5
HM <hello@hazella.co>
2019-03-01 19:33:45 -0600

    Layout updated

--- app/src/main/java/com/hm/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/hm/beeradviser/FindBeerActivity.java
@@ -2,6 +2,7 @@ package com.hm.beeradviser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FindBeerActivity extends Activity {
 
@@ -9,5 +10,10 @@ public class FindBeerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    // Called when the user clicks the button.
+    public void onClickFindBeer(View view) {
+
     }
 }
